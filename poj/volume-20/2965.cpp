55e1989cb8b62feba49c13c62a658447b5570241
openinx <openinx@gmail.com>
2014-08-22 23:57:27 +0800

    poj 2965

---
+++ poj/volume-20/2965.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void solve(int start, int end){
+}
+
+int main(){
+    int cnt = 0, beg = 0, end = ; 
+    while((c = getchar()) && (c == '+' || c == '-')){
+        if(c == '+')
+            beg |= (1<<cnt);
+        ++ cnt;
+        solve(beg, 0);
+    }
+    return 0 ;
+}
