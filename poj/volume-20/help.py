09416db5f07c7f7f470a970fcd9d1204d98fa035
openinx <openinx@gmail.com>
2014-08-24 00:53:06 +0800

    poj 2945

--- poj/volume-20/help.py
+++ poj/volume-20/help.py
@@ -4,7 +4,7 @@ import random
 
 with open('2945.in', 'w') as fd:
     n  = 20000 
-    m = 20 
+    m = 20
     fd.write("%s %s\n" % (n , m))
     for i in xrange(n):
         s = ''.join([random.choice('ACGT') for i in xrange(m)])
